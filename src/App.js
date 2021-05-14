361993b4f7dbdb4cc6aad794a5950fac9a3b54a8
Yathaartha <maharjanyathaartha017@gmail.com>
2021-05-14 19:17:20 +0545

    initialized Dropdown widget

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 const items = [
   {
@@ -17,11 +18,27 @@ const items = [
   },
 ];
 
-const App = () => {
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Blue",
+    value: "blue",
+  },
+  {
+    label: "A Shade of Green",
+    value: "green",
+  },
+];
+
+const App = ({ options }) => {
   return (
     <div>
       {/* <Accordion items={items} /> */}
-      <Search />
+      {/* <Search /> */}
+      <Dropdown options={options} />
     </div>
   );
 };
---
+++ src/components/Dropdown.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Dropdown = ({ options }) => {
+  return <div>Dropdown</div>;
+};
+
+export default Dropdown;
